4d1590c7c2cd548c16e906f5f8c7a01da26a3d7c
wwyyzz <sdn_app@163.com>
2018-12-28 18:00:17 +0800

    1228

--- src/main/java/com/wj/demo/HelloJob.java
+++ src/main/java/com/wj/demo/HelloJob.java
@@ -1,20 +1,57 @@
 package com.wj.demo;
 
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+import org.quartz.*;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.SimpleFormatter;
 
 public class HelloJob implements Job {
+
+
     @Override
-    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+    public void execute(JobExecutionContext context) throws JobExecutionException {
         Date date = new Date();
         SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("==========================================");
         System.out.println("Current Exec Time is: " + sf.format(date));
+
+
+
+
+
+
+
+
+
 //        编写具体的业务逻辑
-        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
+//
+//        JobKey key = context.getJobDetail().getKey();
+//        System.out.println("My job name and group are :" + key.getName() + "   " + key.getGroup());
+//
+//        TriggerKey triggerKey = context.getTrigger().getKey();
+//        System.out.println("My Trigger is:" + triggerKey.getName() + "   " + triggerKey.getGroup());
+//
+//        JobDataMap dataMap = context.getJobDetail().getJobDataMap();
+//        JobDataMap tdataMap = context.getTrigger().getJobDataMap();
+//
+////        System.out.println(dataMap.getString("message"));
+//
+//        String jobMsg = dataMap.getString("message");
+//        Float jobFloatValue = dataMap.getFloat("FloatJobValue");
+//        System.out.println("jobMessage =" + jobMsg);
+//        System.out.println("jobFloatValue =" + jobFloatValue);
+//
+//        System.out.println("triggerMessage =" + tdataMap.getString("message"));
+//        System.out.println("triggerDoubleValue =" + tdataMap.getDouble("DoubbleTriggerValue"));
+//
+//        String triggerMsg = tdataMap.getString("message");
+//        Double triggerDoubleValue = tdataMap.getDouble("DoubleTriggleValue");
+//        System.out.println("triggerMsg =" + triggerMsg.toString());
+//        System.out.println("triggerDoubleValue =" + triggerDoubleValue.toString());
+//
+//        System.out.println("hi!!!!!!!");
+
     }
 }
--- src/main/java/com/wj/demo/HelloScheduler.java
+++ src/main/java/com/wj/demo/HelloScheduler.java
@@ -7,28 +7,66 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class HelloScheduler {
-    public static void main(String[] args) throws SchedulerException{
+    public static void main(String[] args) throws SchedulerException, InterruptedException {
 //        创建一个JobDetail实例，将该实例与HelloJob绑定
-        JobDetail jobDetail = JobBuilder.newJob(HelloJob.class).withIdentity("myjob", "group1").build();
+        JobDetail jobDetail = JobBuilder.newJob(HelloJob.class)
+                .withIdentity("myjob")
+                .usingJobData("message","hello myJob1")
+                .usingJobData("FloatJobValue",3.14F)
+                .build();
+//        System.out.println("job class:" + jobDetail.getJobClass());
+//        System.out.println("job name:" + jobDetail.getKey().getName());
+//        System.out.println("job group:" + jobDetail.getKey().getGroup());
 //        创建一个Trigger实例，定义该job立即执行，每个两秒执行一次
+        Date date = new Date();
+        date.setTime(date.getTime() + 3000);
+
+        Date stopTime = new Date();
+        stopTime.setTime(date.getTime() + 6000);
+
+        //        CronTrigger
+        CronTrigger trigger2 = (CronTrigger) TriggerBuilder
+                .newTrigger()
+                .withIdentity("mytrigger2","group1")
+                .startAt(date)
+                .endAt(stopTime)
+                .usingJobData("message", "hello myTrigger1")
+                .usingJobData("DoubbleTriggerValue", 2.0D)
+                .withSchedule(
+                        CronScheduleBuilder.cronSchedule("* * * * * ? *"))
+                .build();
+
+
+
+//        SimpleTrigger
         Trigger trigger = TriggerBuilder
                 .newTrigger()
-                .withIdentity("mytrigger","group1")
-                .startNow()
+                .withIdentity("mytrigger1","group1")
+                .startAt(date)
+                .endAt(stopTime)
+                .usingJobData("message", "hello myTrigger1")
+                .usingJobData("DoubbleTriggerValue", 2.0D)
                 .withSchedule(
                         SimpleScheduleBuilder.simpleSchedule()
                                 .withIntervalInSeconds(2)
-                                .repeatForever())
+                                .withRepeatCount(50))
                 .build();
 
 //        创建Scheduler实例
         SchedulerFactory sfact = new StdSchedulerFactory();
         Scheduler scheduler = sfact.getScheduler();
         scheduler.start();
-        Date date = new Date();
+
         SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         System.out.println("Current Exec Time is: " + sf.format(date));
-        scheduler.scheduleJob(jobDetail,trigger);
+        System.out.println("scheduler is:" + sf.format(scheduler.scheduleJob(jobDetail,trigger2)));
+
+        Thread.sleep(3000L);
+        scheduler.standby();
+
+        Thread.sleep(3000L);
+        scheduler.start();
+
     }
 
 }
